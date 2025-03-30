08a03ede6d6ec60b44fbc04bf9bce90229d9521a
Гулямов Тимур Икромович <TGulyamov@alfabank.ru>
2025-03-30 22:41:49 +0300

    Added groovy lsp

--- nvim/.config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -218,6 +218,7 @@ return {
             shadow = true,
           },
         },
+        groovyls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
