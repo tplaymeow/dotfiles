b301d198992d42792d24e6355a2f98d9aa869635
Гулямов Тимур Икромович <TGulyamov@alfabank.ru>
2025-04-02 01:55:32 +0300

    Added lsp

--- nvim/.config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -219,17 +219,7 @@ return {
           },
         },
         groovyls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
-
+        jinja_lsp = {},
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
